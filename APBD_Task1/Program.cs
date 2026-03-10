360a1f274a888d1675084ee902664dc17e46a492
Antoni Słomka <s33610@pjwstk.edu.pl>
2026-03-10 13:32:20 +0100

    meaningful commit #2 List

--- APBD_Task1/Program.cs
+++ APBD_Task1/Program.cs
@@ -5,6 +5,14 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello");
+
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("adedd " + i);
+                list.Add(i);
+            }
         }
     }
 }
