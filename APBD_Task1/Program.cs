52222e623fbf7380b49218bf8aaf0e007beeb76f
Antoni Słomka <s33610@pjwstk.edu.pl>
2026-03-15 18:27:23 +0100

    Changed the greeting message to Hello world

--- APBD_Task1/Program.cs
+++ APBD_Task1/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Hello world");
 
             List<int> list = new List<int>();
 
